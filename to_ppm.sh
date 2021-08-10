c0bdf72c36ec6948f7c6a87adcabc82431546275
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-09 22:31:19 -0400

    Make `to_ppm.sh` more robust
    It can now convert any (detectable) image files to the correct format,
    not just `jpg` and `png`. Format is also configurable if needed.`

--- to_ppm.sh
+++ to_ppm.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 if [ -z "$1" ]
-  then
-    echo "Give name of folder to be searhced for images to format"
+then
+  echo "Usage: $0 <directory> <format>"
+  exit 1
 fi
 
-#find $1 -type f -exec echo "put {}" \;
-files=($(find $1 -type f))
-for file in "${files[@]}"
+FORMAT=ppm
+if [ $# -gt 1 ]
+then
+  FORMAT=$2
+fi
+
+function convert_image() {
+  local file=$1
+  echo "Converting $FILE"
+  mogrify -format $FORMAT -quality 100 $file 2>&1 | grep -v "warning"
+  rm $file
+}
+
+for FILE in $(find $1 -type f)
 do
-    #echo "$file"
-    if [ "${file: -4}" == ".jpg" ] || [ "${file: -4}" == ".png" ]
+  if file "$FILE" | grep -qE 'image|bitmap'; then
+    if [[ "$FILE" != *"$FORMAT" ]]
     then
-        echo "$file : Converting image to ppm"
-        mogrify -format ppm -quality 100 $file #2> /dev/null
-        rm "$file"
-    elif [ "${file: -4}" == ".mtl" ]
+      convert_image $FILE &
+    fi
+  elif [ "${FILE: -4}" == ".mtl" ]
+  then
+    if $(grep "map_Kd" "$FILE" | grep -q -v ".$FORMAT")
     then
-        if grep -q -e jpg -e png "$file"
+        echo "Changing image references in $FILE"
+        if [[ "$OSTYPE" == "darwin"* ]]
         then
-            echo "$file : Changing image references to ppm"
-            sed -i -e 's/jpg/ppm/g' -e 's/png/ppm/g' "$file"
+          # MacOS / BSD sed needs some special syntax for in place editing
+          sed -i '' -e "s/\(map_.*\)\.[^.]*$/\1\.$FORMAT/g" "$FILE"
+        else
+          sed -i -e "s/\(map_.*\)\.[^.]*$/\1\.$FORMAT/g" "$FILE"
         fi
     fi
+  fi
 done
 
-echo "All files in $1 formatted correctly"
+wait
+
+echo "Done."
